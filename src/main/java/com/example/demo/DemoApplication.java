3dc5402167776444b15cd001c37d75208d0ae37b
caner akin <caner.akin@secomtr.com>
2022-10-21 16:40:20 +0300

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @PostConstruct
+    public void start(){
+        System.out.println("Hello World");
+    }
+
+}

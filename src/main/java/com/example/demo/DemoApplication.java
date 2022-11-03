deb0884aab7eaa4ef96c10f3881367f504234b7c
caner akin <caner.akin@secomtr.com>
2022-11-03 09:14:42 +0300

    servlet

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
 import java.sql.SQLOutput;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+
+    // 404 gidermek için servler initial edildi ve builder override edildi
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
